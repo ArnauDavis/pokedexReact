592e00a84a8ea880cef1ece9f4fdd45bfdd82d3b
Arnau Davis <arnau.davis@outlook.com>
2025-11-29 20:14:00 -0500

    successfully moved fetch to parent, data can now be moved between main screen and search bar, cries have yet to be added

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -3,14 +3,105 @@ import staticTv from "../assets/imgs/static.gif"
 import SearchBar from "./SearchBar"
 export default function MainScreen() {
 
+  const [pokemonName, setPokemonName] = useState('')
+  const handleInputChange = (event) => {
+        setPokemonName(event.target.value.toLowerCase()); 
+    }
   const [screen, setScreen] = useState(staticTv)
+  const [firstApiResponse, setfirstApiResponse] = useState(null)
+  const [secondApiResponse, setSecondApiResponse] = useState(null)
+  const [pokeHeight, setpokeHeight] = useState(null)
+  const [pokemonNumber, setPokemonNumber] = useState(null)
+  const [pokeName, setPokeName] = useState(null)
+  const [pokePic, setPokePic] = useState(null)
+  const [pokeSecondPic, setPokeSecondPic] = useState(null)
+  const [pokeWeight, setpokeWeight] = useState(null)
+  const [pokeDescription, setPokeDescription] = useState(null)
+  
+  
+  const [pokeType, setpokeType] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
+    const fetchPokemon = async () => {
+      // --- Step 1: Initialization ---
+    setIsLoading(true)
+    setError(null)
+    setfirstApiResponse(null)
+    setSecondApiResponse(null)
+      try {
+    // --- Step 2: FIRST FETCH (Get general poke data) ---
+    //from this fetch we will get cries, height, id, name, pictures, type, and weight
+    const pokeApiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    
+    if (!pokeApiResponse.ok) {
+      throw new Error(`first fetch failed: ${pokeApiResponse.status}`);
+    }
+    
+    const dataFromFirstApi = await pokeApiResponse.json();
+    setfirstApiResponse(dataFromFirstApi) // Save the result of the first fetch
+    setpokeHeight(dataFromFirstApi.height)
+    setPokemonNumber(dataFromFirstApi.id)
+    setPokeName(dataFromFirstApi.name.toUpperCase())
+    setPokePic(dataFromFirstApi.sprites.front_default)
+    if(dataFromFirstApi.sprites.front_female){
+      setPokeSecondPic(dataFromFirstApi.sprites.front_female)
+    }else if(!dataFromFirstApi.sprites.front_female){
+      setPokeSecondPic(dataFromFirstApi.sprites.back_default)
+    }
+    setpokeWeight(dataFromFirstApi.weight)
+    setpokeType(dataFromFirstApi.types.map((x)=>x.type.name).join(' '))
+    // now we can get the poke id to get more info in second fetch
+    const pokeId = dataFromFirstApi.id;
+    // --- Step 3: SECOND FETCH (get more data from a different url using the poke id) ---
+    // this fetch will give us a description of the pokemon
+    const secondPokeApiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokeId}/`);
+    
+    if (!secondPokeApiResponse.ok) {
+      throw new Error(`second fetch failed: ${secondPokeApiResponse.status}`);
+    }
+    const dataFromSecondApi = await secondPokeApiResponse.json();
+    setSecondApiResponse(dataFromSecondApi) // Save the result of the second fetch
+    console.log(dataFromSecondApi.flavor_text_entries[0].flavor_text)
+    setPokeDescription(dataFromSecondApi.flavor_text_entries[0].flavor_text)
+  } catch (err) {
+    // --- Step 4: Error Handling ---
+    console.error("Sequential fetching error:", err);
+    setError(`Data load error: ${err.message}`);
+  } finally {
+    // --- Step 5: Finalization ---
+    setIsLoading(false);
+  }
+    }
   
   return (
     <>
+    <div>
+      { firstApiResponse &&
+      <div>
+      <h3>Putting variables here</h3>
+      <span>{pokeHeight/10} m</span>
+      <br />
+      <span>{pokemonNumber}</span>
+      <br />
+      <span>{pokeName}</span>
+      <br />
+      <span>{pokeType}</span>
+      <br />
+      <span><img src={pokePic} alt="pokePic" /></span>
+      <br />
+       <span><img src={pokeSecondPic} alt="pokePic" /></span>
+      <br />
+      <span>{pokeWeight/10} lbs.</span>
+      <br />
+      {secondApiResponse?  <span>{pokeDescription}</span> : <span>No data available</span> }
+      </div>
+              }
+      {!firstApiResponse && <span>{error}</span>}
+    </div> 
     <div className="h-80 mt-5 flex justify-center bg-white/60 backdrop-blur-md p-1 rounded-xl shadow-lg border-4 border-black shadow-[inset_0_0_0_4px_white]">
         <img className="w-full h-full object-fill rounded-l" src={screen} alt="pokeScreen" />
     </div>
-    <SearchBar/>
+    <SearchBar pokemonName={pokemonName} handleInputChange={handleInputChange} triggerFetch={fetchPokemon} isFetching={isLoading}/>
     </>
   )
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,107 +1,16 @@
 import { useState } from 'react'
 import pokeball from "../assets/imgs/pokeball.png"
 
-export default function SearchBar(){
-    const [pokemonName, setPokemonName] = useState('')
-    const [firstApiResponse, setfirstApiResponse] = useState(null)
-    const [secondApiResponse, setSecondApiResponse] = useState(null)
-    const [pokeHeight, setpokeHeight] = useState(null)
-    const [pokemonNumber, setPokemonNumber] = useState(null)
-    const [pokeName, setPokeName] = useState(null)
-    const [pokePic, setPokePic] = useState(null)
-    const [pokeSecondPic, setPokeSecondPic] = useState(null)
-    const [pokeWeight, setpokeWeight] = useState(null)
-    const [pokeDescription, setPokeDescription] = useState(null)
-   
-   
-    const [pokeType, setpokeType] = useState(null)
-    const [isLoading, setIsLoading] = useState(false)
-    const [error, setError] = useState(null)
-    const handleInputChange = (event) => {
-        setPokemonName(event.target.value.toLowerCase()); 
-    }
-
-      const fetchPokemon = async () => {
-        // --- Step 1: Initialization ---
-      setIsLoading(true)
-      setError(null)
-      setfirstApiResponse(null)
-      setSecondApiResponse(null)
-        try {
-      // --- Step 2: FIRST FETCH (Get general poke data) ---
-      //from this fetch we will get cries, height, id, name, pictures, type, and weight
-      const pokeApiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-      
-      if (!pokeApiResponse.ok) {
-        throw new Error(`first fetch failed: ${pokeApiResponse.status}`);
-      }
-      
-      const dataFromFirstApi = await pokeApiResponse.json();
-      setfirstApiResponse(dataFromFirstApi) // Save the result of the first fetch
-      setpokeHeight(dataFromFirstApi.height)
-      setPokemonNumber(dataFromFirstApi.id)
-      setPokeName(dataFromFirstApi.name.toUpperCase())
-      setPokePic(dataFromFirstApi.sprites.front_default)
-      if(dataFromFirstApi.sprites.front_female){
-        setPokeSecondPic(dataFromFirstApi.sprites.front_female)
-      }else if(!dataFromFirstApi.sprites.front_female){
-        setPokeSecondPic(dataFromFirstApi.sprites.back_default)
-      }
-      setpokeWeight(dataFromFirstApi.weight)
-      setpokeType(dataFromFirstApi.types.map((x)=>x.type.name).join(' '))
-      // now we can get the poke id to get more info in second fetch
-      const pokeId = dataFromFirstApi.id;
-      // --- Step 3: SECOND FETCH (get more data from a different url using the poke id) ---
-      // this fetch will give us a description of the pokemon
-      const secondPokeApiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokeId}/`);
-      
-      if (!secondPokeApiResponse.ok) {
-        throw new Error(`second fetch failed: ${secondPokeApiResponse.status}`);
-      }
-
-      const dataFromSecondApi = await secondPokeApiResponse.json();
-      setSecondApiResponse(dataFromSecondApi) // Save the result of the second fetch
-      console.log(dataFromSecondApi.flavor_text_entries[0].flavor_text)
-      setPokeDescription(dataFromSecondApi.flavor_text_entries[0].flavor_text)
-    } catch (err) {
-      // --- Step 4: Error Handling ---
-      console.error("Sequential fetching error:", err);
-      setError(`Data load error: ${err.message}`);
-    } finally {
-      // --- Step 5: Finalization ---
-      setIsLoading(false);
-    }
-
-      }
+export default function SearchBar({pokemonName, handleInputChange, triggerFetch, isFetching}){
+  
+    
   
    
 
     
     return (
     <>
-     <div>
-      { firstApiResponse &&
-      <div>
-      <h3>Putting variables here</h3>
-      <span>{pokeHeight/10} m</span>
-      <br />
-      <span>{pokemonNumber}</span>
-      <br />
-      <span>{pokeName}</span>
-      <br />
-      <span>{pokeType}</span>
-      <br />
-      <span><img src={pokePic} alt="pokePic" /></span>
-      <br />
-       <span><img src={pokeSecondPic} alt="pokePic" /></span>
-      <br />
-      <span>{pokeWeight/10} lbs.</span>
-      <br />
-      {secondApiResponse?  <span>{pokeDescription}</span> : <span>No data available</span> }
-      </div>
-              }
-      {!firstApiResponse && <span>{error}</span>}
-    </div> 
+     
 
     <div className="mt-5 w-full max-w-md mx-auto font-mono">
       <div
@@ -154,9 +63,8 @@ export default function SearchBar(){
                     <span><span className="text-red-400">Tip:</span> add -gmax</span>
                 </p>
             <button className="fetch-button bg-red-950 text-red-400 border border-red-400 border-b-4 font-medium overflow-hidden relative px-2 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group"
-                onClick={fetchPokemon} 
-                disabled={isLoading}
-                >{isLoading ? 'Searching...' : `Search`}
+                onClick={triggerFetch} 
+                >{isFetching ? 'Searching...' : `Search`}
                 <span className="bg-red-400 shadow-red-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]">
                 </span>
                 
