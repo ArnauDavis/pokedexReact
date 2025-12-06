85efe1a364668f206444f306c430e887a3acb659
Arnau Davis <arnau.davis@outlook.com>
2025-12-06 16:14:19 -0500

    fixed some styling for mobile

--- src/components/PokedexFrame.jsx
+++ src/components/PokedexFrame.jsx
@@ -3,7 +3,7 @@ import MainScreen from "./MainScreen"
 export default function PokedexFrame(){
     return (
         <>
-        <div className="w-[30rem] bg-red-700 px-10 py-2 rounded-md mt-1">
+        <div className="w-[30rem] bg-red-700 px-5 pt-2 pb-16 rounded-md mt-1">
             <MainScreen/>
         </div>
         </>
